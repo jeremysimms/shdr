c637d4e72c7ca363e394b8373e7c6c47dfa6c473
Jeremy Simms <jeremyrs@gmail.com>
2023-03-19 15:31:13 -0400

    fix

--- include/const.h
+++ include/const.h
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 
 #if defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__)
-    static const SDL_GLprofile SHDR_GL_CONTEXT_MASK = SDL_GL_CONTEXT_EGL;
+    static const SDL_GLprofile SHDR_GL_PROFILE = SDL_GL_CONTEXT_PROFILE_ES;
     static const int SHDR_OPENGL_MAJOR_VERSION = 2;
     static const int SHDR_OPENGL_MINOR_VERSION = 0;
     static const Uint32 SHDR_WINDOW_FLAGS = SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN;
